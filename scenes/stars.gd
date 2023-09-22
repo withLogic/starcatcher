83f8e529051d44adad999bc79492763df5ddb960
withlogic <matt@withlogic.com>
2023-09-21 23:15:07 -0500

    Starcatcher Vita Port
    Port of the game Starcatcher to the PlayStatoin Vita.

--- scenes/stars.gd
+++ scenes/stars.gd
@@ -3,8 +3,9 @@ extends Node2D
 const scnStar := preload("res://scenes/sprStar.tscn")
 
 func _ready() -> void:
-	for _i in range(90):
-		var i:SprStar = scnStar.instance()
-		add_child(i)
-		i.global_position.x = rand_range(0,315)
-		i.global_position.y = rand_range(0,250)
+	pass
+	#for _i in range(90):
+	#	var i:SprStar = scnStar.instance()
+	#	add_child(i)
+	#	i.global_position.x = rand_range(0,315)
+	#	i.global_position.y = rand_range(0,250)
